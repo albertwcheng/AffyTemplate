57e7a1fe9ddd5dc0dc46bdc0f71a84322110c7df
albertwcheng <albertwcheng@gmail.com>
2012-11-06 03:06:42 -0500

    commit

--- scripts/createMAS5OnlyPresenceData.sh
+++ scripts/createMAS5OnlyPresenceData.sh
@@ -4,3 +4,4 @@ cd ..
 cd processed
 
 operateOnMatrices.py -O "X[0] if X[1]=='P' else 'NA'" mas5log2.hgu133plus2hsensgcdf.geneName.TXT mas5calls.hgu133plus2hsensgcdf.geneName.TXT > mas5log2.hgu133plus2hsensgcdf.geneName.P.TXT
+pyFilter.py mas5log2.hgu133plus2hsensgcdf.geneName.P.TXT "'NA' not in [2-_1]" > mas5log2.hgu133plus2hsensgcdf.geneName.NoNARows.TXT
